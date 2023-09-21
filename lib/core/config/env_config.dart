fbe5b3ce54a7da6dabf79112bdc1b32bc6602710
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-21 18:21:28 +0600

    added config(s) added to the app

---
+++ lib/core/config/env_config.dart
@@ -0,0 +1,27 @@
+import 'package:logger/logger.dart';
+
+class EnvConfig {
+  final String appName;
+  final String appVersion;
+  final String baseUrl;
+  final bool shouldCollectCrashLog;
+
+  late final Logger logger;
+
+  EnvConfig({
+    required this.appName,
+    required this.appVersion,
+    required this.baseUrl,
+    this.shouldCollectCrashLog = false,
+  }) {
+    logger = Logger(
+      printer: PrettyPrinter(
+        methodCount: 2,
+        errorMethodCount: 8,
+        lineLength: 120,
+        colors: true,
+        printTime: true,
+      ),
+    );
+  }
+}
