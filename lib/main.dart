f4612fc8b8a174bbcbd16d22c5cb5a2f4ee6ef41
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-22 18:19:35 +0600

    BuildConfig intantiate added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:weather_app/modules/application.dart';
 
-void main() {
+import 'core/config/config.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  EnvConfig devConfig = EnvConfig(
+    appName: packageInfo.appName,
+    appVersion: packageInfo.version,
+    baseUrl: "https://api.weatherapi.com/v1",
+    shouldCollectCrashLog: true,
+  );
+
+  BuildConfig.instantiate(
+    type: Environment.DEVELOPMENT,
+    config: devConfig,
+  );
+
   runApp(
     const WeatherApplication(),
   );
