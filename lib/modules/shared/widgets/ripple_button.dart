32b0025f80aac43dd7a82adeb198a1bd7ec4e08b
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-23 07:54:22 +0600

    widget ripple_button added

---
+++ lib/modules/shared/widgets/ripple_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/core/constants/app_values.dart';
+
+class RippleButton extends StatelessWidget {
+  final Function()? onTap;
+  final Widget? child;
+  final Color? rippleColor;
+  final double rippleRadius;
+
+  const RippleButton({
+    Key? key,
+    this.child,
+    required this.onTap,
+    this.rippleColor,
+    this.rippleRadius = AppValues.radiusSmall,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(rippleRadius),
+        highlightColor: rippleColor ?? Theme.of(context).splashColor,
+        onTap: onTap,
+        child: child,
+      ),
+    );
+  }
+}
