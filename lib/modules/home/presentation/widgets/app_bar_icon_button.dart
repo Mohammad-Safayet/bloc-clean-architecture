2c1371caae6e91d9a16a52ee68f9bfc935235d10
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-23 07:56:42 +0600

    widget app_bar_icon_button for home appbar added

---
+++ lib/modules/home/presentation/widgets/app_bar_icon_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:weather_app/core/constants/app_values.dart';
+import 'package:weather_app/modules/shared/mixin/base_widget.dart';
+
+class AppBarIconButtonWidget extends StatelessWidget with BaseWidgetMixin {
+  AppBarIconButtonWidget({
+    Key? key,
+    required this.onPressed,
+    required this.icon,
+  }) : super(key: key);
+
+  final Function onPressed;
+  final IconData icon;
+
+  @override
+  Widget? body(BuildContext context) {
+    return IconButton(
+      onPressed: () => onPressed,
+      icon: FaIcon(
+        icon,
+        color: Theme.of(context).colorScheme.onSurfaceVariant,
+        size: AppValues.icon_18,
+      ),
+    );
+  }
+}
