e5a3847bbd6223d69ea2100cd973f4b4f58119e7
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-23 07:58:24 +0600

    changed implementation of the home_screen view

--- lib/modules/home/presentation/views/home_screen.dart
+++ lib/modules/home/presentation/views/home_screen.dart
@@ -2,48 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:weather_app/modules/home/presentation/widgets/weather_app_bar.dart';
+import 'package:weather_app/modules/shared/base/base_screen.dart';
 import 'package:weather_app/modules/weather/presentation/views/weather_screen.dart';
 
-class HomeScreenView extends StatelessWidget {
-  HomeScreenView({Key? key}) : super(key: key);
-  final GlobalKey<ScaffoldState> globalKey = GlobalKey<ScaffoldState>();
+class HomeScreenView extends BaseScreen {
+  HomeScreenView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return _annotatedRegion(context);
-  }
-
-  Widget _annotatedRegion(BuildContext context) {
-    return AnnotatedRegion(
-      value: SystemUiOverlayStyle(
-        //Status bar color for android
-        statusBarColor: Theme.of(context).colorScheme.surfaceVariant,
-        statusBarIconBrightness: Brightness.dark,
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: _pageScaffold(context),
-      ),
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return const WeatherAppBarWidget(
+      appTitle: "lak",
     );
   }
 
-  Widget _pageScaffold(BuildContext context) {
-    return Scaffold(
-      key: globalKey,
-      backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
-      appBar: _appBar(context),
-      body: pageContent(context),
-    );
+  @override
+  Widget body(BuildContext context) {
+    return WeatherScreenView();
   }
-
-  PreferredSizeWidget? _appBar(BuildContext context) {
-    return const WeatherAppBarWidget(appTitle: "appTitle");
-  }
-
-  Widget pageContent(BuildContext context) {
-    return const SafeArea(
-      child: WeatherScreenView(),
-    );
-  }
-
 }
