9b5aafdb2ec4cb0a140f2b4a41f40fabab7bdfa4
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-25 08:17:36 +0600

    extension on weather_location added

---
+++ lib/core/extensions/models/weather_location_extension.dart
@@ -0,0 +1,18 @@
+import 'package:intl/intl.dart';
+import 'package:weather_app/core/constants/app_values.dart';
+import 'package:weather_app/modules/weather/domain/entities/weather_location.dart';
+import 'package:weather_app/modules/weather/infra/models/weather_location_model.dart';
+
+extension WeatherLocationModelExtension on WeatherLocationModel {
+  WeatherLocation toEntity() {
+    return WeatherLocation(
+      name: name,
+      region: region,
+      country: country,
+      latitude: latitude,
+      longitude: longitude,
+      tzId: tzId,
+      localTime: DateFormat(AppValues.yyyyMMddHHmm).parseLoose(localTime),
+    );
+  }
+}
