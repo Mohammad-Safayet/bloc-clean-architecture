849e0c31ebfc5cf74d8e7df03e499b91e343bde1
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-25 08:18:09 +0600

    extension on astro_model added

---
+++ lib/core/extensions/models/astro_model_extension.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+import 'package:logger/logger.dart';
+
+import 'package:weather_app/modules/weather/domain/entities/astro.dart';
+import 'package:weather_app/modules/weather/infra/models/astro_model.dart';
+
+extension AstroModelExtension on AstroModel {
+  Astro toEntity() {
+    return Astro(
+      sunRise: DateFormat.jm().parseLoose(sunRise),
+      sunSet: DateFormat.jm().parseLoose(sunSet),
+      moonRise: DateFormat.jm().parseLoose(moonRise),
+      moonSet: DateFormat.jm().parseLoose(moonSet),
+      moonPhase: moonPhase,
+      moonIllumination: int.parse(moonIllumination),
+      isMoonUp: isMoonUp == 1,
+      isSunUp: isSunUp == 1,
+    );
+  }
+}
