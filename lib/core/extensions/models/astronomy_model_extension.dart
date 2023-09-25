d5aff85c07e398e4e1b6f86b19c399a2712740a4
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-25 08:19:27 +0600

    refactored

--- lib/core/extensions/models/astronomy_model_extension.dart
+++ lib/core/extensions/models/astronomy_model_extension.dart
@@ -1,5 +1,5 @@
 import 'package:weather_app/core/extensions/models/astro_model_extension.dart';
-import 'package:weather_app/core/extensions/models/weather_location_extension.dart';
+import 'package:weather_app/core/extensions/models/weather_location_model_extension.dart';
 import 'package:weather_app/modules/weather/domain/entities/astronomy.dart';
 import 'package:weather_app/modules/weather/infra/models/astronomy_model.dart';
 
