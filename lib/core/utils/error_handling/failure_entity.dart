cc432ea682a3ecf9921d8baf117ad711dda1390e
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-25 08:15:08 +0600

    failure_entity added

---
+++ lib/core/utils/error_handling/failure_entity.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'failure_entity.freezed.dart';
+
+@freezed
+class FailureEntity with _$FailureEntity {
+  const FailureEntity._();
+
+  const factory FailureEntity.applicationFailure() = ApplicationFailure;
+
+  const factory FailureEntity.jsonFormatFailure() = JsonFormatFailure;
+
+  const factory FailureEntity.networkingFailure() = NetworkingFailure;
+
+  const factory FailureEntity.notFoundFailure() = NotFoundFailure;
+
+  const factory FailureEntity.serviceUnavailableFailure() =
+      ServiceUnavailableFailure;
+
+  const factory FailureEntity.timeoutFailure() = TimeoutFailure;
+
+  const factory FailureEntity.unauthorizedFailure() = UnauthorizedFailure;
+}
