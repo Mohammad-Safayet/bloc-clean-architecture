dce7509492b1ac3220ef109bd78a211d1b15003f
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-26 17:00:41 +0600

    qurey parameter added

--- lib/modules/weather/infra/datasources/remote/weather_remote_datasource.dart
+++ lib/modules/weather/infra/datasources/remote/weather_remote_datasource.dart
@@ -1,7 +1,10 @@
+import 'package:weather_app/modules/weather/domain/entities/astronomy_query_param.dart';
 import 'package:weather_app/modules/weather/infra/models/astronomy_model.dart';
 
 abstract class WeatherRemoteDataSource {
   Future getCurrentWeatherData();
 
-  Future<AstronomyModel> getAstronomyData();
+  Future<AstronomyModel> getAstronomyData(
+    AstronomyQueryParam queryParam,
+  );
 }
--- lib/modules/weather/infra/datasources/remote/weather_remote_datasource_impl.dart
+++ lib/modules/weather/infra/datasources/remote/weather_remote_datasource_impl.dart
@@ -1,14 +1,21 @@
 import 'package:weather_app/modules/shared/base/base_remote_datasource.dart';
-import 'package:weather_app/modules/weather/infra/datasources/remote/weather_remote_datasource.dart';
 import 'package:weather_app/modules/weather/infra/models/astronomy_model.dart';
+import 'package:weather_app/modules/weather/domain/entities/astronomy_query_param.dart';
+import 'package:weather_app/core/extensions/models/astronomy_query_param_extension.dart';
+import 'package:weather_app/modules/weather/infra/datasources/remote/weather_remote_datasource.dart';
 
 class WeatherRemoteDataSourceImpl extends BaseRemoteDataSource
     implements WeatherRemoteDataSource {
   @override
-  Future<AstronomyModel> getAstronomyData() async {
-    final endpoint =
-        "${dioClient.options.baseUrl}/astronomy.json?key=f5175a14c4dd4efbb5b04128231709&q=22.3752,91.8349&dt=2023-09-21";
-    final api = dioClient.get(endpoint);
+  Future<AstronomyModel> getAstronomyData(
+    AstronomyQueryParam queryParam,
+  ) async {
+    final endpoint = "${dioClient.options.baseUrl}/astronomy.json?";
+    
+    final api = dioClient.get(
+      endpoint,
+      queryParameters: queryParam.toQueryParam(),
+    );
 
     try {
       final response = await callApiWithErrorParser(api);
