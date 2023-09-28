8aba0c85cf395ea32a01c424d2d146ba58dc6078
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-28 08:17:51 +0600

    added locationPermissionDeniedFailure entity added

--- lib/core/utils/error_handling/failure_entity.dart
+++ lib/core/utils/error_handling/failure_entity.dart
@@ -7,42 +7,47 @@ class FailureEntity with _$FailureEntity {
   const FailureEntity._();
 
   const factory FailureEntity.applicationFailure({
+    required String title,
     required String message,
   }) = ApplicationFailure;
 
+  const factory FailureEntity.locationPermissionFailure({
+    required String title,
+    required String message,
+  }) = LocationPermissionFailure;
+
   const factory FailureEntity.apiFailure({
-    required int httpCode,
-    required String status,
+    required String title,
     required String message,
   }) = ApiFailure;
 
   const factory FailureEntity.jsonFormatFailure({
+    required String title,
     required String message,
   }) = JsonFormatFailure;
 
   const factory FailureEntity.networkingFailure({
+    required String title,
     required String message,
   }) = NetworkingFailure;
 
   const factory FailureEntity.notFoundFailure({
-    required int httpCode,
-    required String status,
+    required String title,
     required String message,
   }) = NotFoundFailure;
 
   const factory FailureEntity.serviceUnavailableFailure({
-    required int httpCode,
-    required String status,
+    required String title,
     required String message,
   }) = ServiceUnavailableFailure;
 
   const factory FailureEntity.timeoutFailure({
+    required String title,
     required String message,
   }) = TimeoutFailure;
 
   const factory FailureEntity.unauthorizedFailure({
-    required int httpCode,
-    required String status,
+    required String title,
     required String message,
   }) = UnauthorizedFailure;
 }
