11e2801f2480b2503e654c6324a5772d6e6ab148
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-30 08:47:29 +0600

    fix: unused import

--- lib/modules/home/presentation/widgets/weather_app_bar.dart
+++ lib/modules/home/presentation/widgets/weather_app_bar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:weather_app/modules/home/presentation/widgets/app_bar_icon_button.dart';
-import 'package:weather_app/modules/home/presentation/widgets/app_bar_icon_button.dart';
-import 'package:weather_app/modules/home/presentation/widgets/app_bar_icon_button.dart';
 import 'package:weather_app/modules/shared/widgets/application_bar.dart';
 
 class WeatherAppBarWidget extends StatelessWidget
