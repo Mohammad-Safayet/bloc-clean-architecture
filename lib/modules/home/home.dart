8c4a491cd07323dcb36b411e0c4e80e548fd54ee
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-30 18:14:44 +0600

    HomePage added

---
+++ lib/modules/home/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:weather_app/modules/home/presentation/views/home_screen.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreenView();
+  }
+}
