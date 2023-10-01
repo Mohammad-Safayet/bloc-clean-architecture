2c909988d272c7e62399ef2b82dec5635b5bb8aa
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-01 17:15:10 +0600

    implemented HomeScreenView

--- lib/modules/home/presentation/views/home_screen.dart
+++ lib/modules/home/presentation/views/home_screen.dart
@@ -1,46 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:weather_app/core/routes/app_pages.dart';
-import 'package:weather_app/modules/home/presentation/bloc/location_bloc.dart';
 
-import 'package:weather_app/modules/home/presentation/widgets/weather_app_bar.dart';
-import 'package:weather_app/modules/shared/base/base_screen.dart';
-import 'package:weather_app/modules/shared/base/base_state.dart';
+import 'package:weather_app/core/routes/app_pages.dart';
+import 'package:weather_app/core/utils/error_handling/failure_entity.dart';
+import 'package:weather_app/modules/home/presentation/bloc/location_bloc.dart';
+import 'package:weather_app/modules/shared/base/base_view.dart';
 import 'package:weather_app/modules/shared/widgets/loading.dart';
 import 'package:weather_app/modules/weather/presentation/views/weather_screen.dart';
 
-class HomeScreenView extends BaseScreen {
-  HomeScreenView({super.key});
-
+class HomeScreenView extends BaseView<LocationBloc> {
   @override
-  PreferredSizeWidget? appBar(BuildContext context) {
-    return const WeatherAppBarWidget(
-      appTitle: "lak",
+  void error(BuildContext context, FailureEntity entity) {
+    context.go(
+      Routes.ERROR,
+      extra: entity,
     );
   }
 
   @override
-  Widget body(BuildContext context) {
-    return BlocConsumer<LocationBloc, BaseState>(
-      listener: (context, state) {
-        if(state is Error) {
-          context.go(Routes.ERROR, extra: state.error);
-        }
-      },
-      builder: (context, state) {
-        final locationBloc = context.read<LocationBloc>();
+  Widget initial(LocationBloc bloc) {
+    bloc.add(const RequestLocationPermission());
 
-        if(state is Initial) {
-          locationBloc.add(const RequestLocationPermission());
+    return const LoadingWidget();
+  }
 
-          return const LoadingWidget();
-        } else if (state is Success) {
-          return const  WeatherScreenView();
-        } else {
-          return const LoadingWidget();
-        }
-      },
-    );
+  @override
+  Widget success(LocationBloc bloc) {
+    return const WeatherScreenView();
   }
 }
