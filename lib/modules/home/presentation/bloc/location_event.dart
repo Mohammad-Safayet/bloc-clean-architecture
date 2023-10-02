8df751ec167672de8902041fab98ae935b6333aa
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-02 20:24:51 +0600

    removed unnecessary event

--- lib/modules/home/presentation/bloc/location_event.dart
+++ lib/modules/home/presentation/bloc/location_event.dart
@@ -2,9 +2,5 @@ part of 'location_bloc.dart';
 
 @freezed
 class LocationEvent with _$LocationEvent {
-  const factory LocationEvent.started() = Started;
-
-  const factory LocationEvent.getPermission() = RequestLocationPermission;
-
-  const factory LocationEvent.getLocation() = RequestLocation;
+  const factory LocationEvent.getCurrentLocation() = RequestCurrentLocation;
 }
