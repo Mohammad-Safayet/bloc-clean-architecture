84d28c5b1e10456667b537375b283c87fc91f1a7
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-03 07:25:56 +0600

    astronomy_remote_datasource added and implemented

---
+++ lib/modules/weather/astronomy/infra/datasources/astronomy_remote_datasource.dart
@@ -0,0 +1,29 @@
+import 'package:weather_app/modules/shared/base/base_remote_datasource.dart';
+import 'package:weather_app/core/extensions/models/astronomy_query_param_extension.dart';
+import 'package:weather_app/modules/weather/astronomy/infra/models/astronomy_model.dart';
+import 'package:weather_app/modules/weather/infra/datasources/weather_remote_datasource.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy_query_param.dart';
+
+class AstronomyRemoteDataSource extends BaseRemoteDataSource
+    implements WeatherRemoteDataSource<AstronomyModel, AstronomyQueryParam> {
+  @override
+  Future<AstronomyModel> getData(
+    AstronomyQueryParam queryParam,
+  ) async {
+    final endpoint = "${dioClient.options.baseUrl}/astronomy.json?";
+
+    final api = dioClient.get(
+      endpoint,
+      queryParameters: queryParam.toQueryParam(),
+    );
+
+    try {
+      final response = await callApiWithErrorParser(api);
+      final data = response.data;
+
+      return AstronomyModel.fromJson(data);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
