cf0e4a87efd23c4e1ce1f19421c705974647625a
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-03 07:23:36 +0600

    refactored astronomy usecase

--- lib/modules/weather/astronomy/domain/usecases/get_astronomy_data_usecase.dart
+++ lib/modules/weather/astronomy/domain/usecases/get_astronomy_data_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:weather_app/core/utils/error_handling/error_handling.dart';
 import 'package:weather_app/modules/shared/base/base_usecase.dart';
-import 'package:weather_app/modules/weather/domain/entities/astronomy.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy.dart';
 import 'package:weather_app/modules/weather/infra/repositories/weather_repo.dart';
 
 class GetAstronomyDataUsecase extends BaseUsecase<Astronomy> {
