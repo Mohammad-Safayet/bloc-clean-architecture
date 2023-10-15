cb799113c7c07c3282feac76adcf53601745f57b
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-15 07:12:29 +0600

    added weather page

---
+++ lib/modules/weather/weather.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'package:weather_app/modules/shared/base/base_screen.dart';
+import 'package:weather_app/modules/weather/main/presentation/bloc/connectivity_bloc.dart';
+import 'package:weather_app/modules/weather/main/presentation/views/weather_screen.dart';
+
+class WeatherPage extends BaseScreen {
+  final Position position;
+
+  WeatherPage({
+    super.key,
+    required this.position,
+  });
+
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) {
+        return ConnectivityBloc();
+      },
+      child: WeatherScreenView(
+        position: position,
+      ),
+    );
+  }
+}
