77ff33103f9713f3e9b56efed0b4ea71a8ed95ac
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-15 07:52:08 +0600

    changed from BlocProvider to MultiBlocProvider

--- lib/modules/weather/weather.dart
+++ lib/modules/weather/weather.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 
 import 'package:weather_app/modules/shared/base/base_screen.dart';
+import 'package:weather_app/modules/weather/astronomy/presentation/bloc/astronomy_bloc.dart';
 import 'package:weather_app/modules/weather/main/presentation/bloc/connectivity_bloc.dart';
 import 'package:weather_app/modules/weather/main/presentation/views/weather_screen.dart';
 
@@ -21,10 +22,12 @@ class WeatherPage extends BaseScreen {
 
   @override
   Widget body(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) {
-        return ConnectivityBloc();
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ConnectivityBloc(),
+        ),
+      ],
       child: WeatherScreenView(
         position: position,
       ),
