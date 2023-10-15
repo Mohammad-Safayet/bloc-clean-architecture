d337c0fe55b0a0cfe731077aff48d3ed08726282
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-15 07:47:42 +0600

    removed WeatherScreenView and added WeatherPage

--- lib/modules/home/presentation/views/home_screen.dart
+++ lib/modules/home/presentation/views/home_screen.dart
@@ -7,11 +7,14 @@ import 'package:weather_app/core/utils/error_handling/failure_entity.dart';
 import 'package:weather_app/modules/home/presentation/bloc/location_bloc.dart';
 import 'package:weather_app/modules/shared/base/base_view.dart';
 import 'package:weather_app/modules/shared/widgets/loading.dart';
-import 'package:weather_app/modules/weather/main/presentation/views/weather_screen.dart';
+import 'package:weather_app/modules/weather/weather.dart';
 
 class HomeScreenView extends BaseView<LocationBloc, Position> {
   @override
-  void error(BuildContext context, FailureEntity entity) {
+  void error(
+    BuildContext context,
+    FailureEntity entity,
+  ) {
     context.go(
       Routes.ERROR,
       extra: entity,
@@ -19,15 +22,22 @@ class HomeScreenView extends BaseView<LocationBloc, Position> {
   }
 
   @override
-  Widget initial(LocationBloc bloc) {
+  Widget initial(
+    BuildContext context,
+    LocationBloc bloc,
+  ) {
     bloc.add(const RequestCurrentLocation());
 
     return const LoadingWidget();
   }
 
   @override
-  Widget success(LocationBloc bloc, Position data) {
-    return WeatherScreenView(
+  Widget success(
+    BuildContext context,
+    LocationBloc bloc,
+    Position data,
+  ) {
+    return WeatherPage(
       position: data,
     );
   }
