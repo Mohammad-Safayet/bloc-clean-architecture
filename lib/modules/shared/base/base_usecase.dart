fb6ee7cbb4aedc690559e0d950cc440a3f1ccec1
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-16 06:43:20 +0600

    added parameter for usecase call method

--- lib/modules/shared/base/base_usecase.dart
+++ lib/modules/shared/base/base_usecase.dart
@@ -3,10 +3,10 @@ import 'package:weather_app/core/config/build_config.dart';
 import 'package:weather_app/core/extensions/failure_entity_extension.dart';
 import 'package:weather_app/core/utils/error_handling/error_handling.dart';
 
-abstract class BaseUsecase<T extends Object> {
+abstract class BaseUsecase<T extends Object, Q extends Object?> {
   final logger = BuildConfig.instance.envConfig.logger;
 
-  Future<Either<FailureEntity, T>> call();
+  Future<Either<FailureEntity, T>> call(Q query);
 
   FailureEntity mapExceptionToFailureEntity(Exception exception) {
     if (exception is BaseException) {
