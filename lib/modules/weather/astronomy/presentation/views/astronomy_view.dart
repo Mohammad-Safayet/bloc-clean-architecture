3e7a3fd7913e5c413b98d2d5606603ccbad07963
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-16 07:05:58 +0600

    code cleanup

--- lib/modules/weather/astronomy/presentation/views/astronomy_view.dart
+++ lib/modules/weather/astronomy/presentation/views/astronomy_view.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
 import 'package:weather_app/core/constants/app_values.dart';
-import 'package:weather_app/modules/shared/mixin/base_widget.dart';
+import 'package:weather_app/core/routes/app_pages.dart';
+import 'package:weather_app/core/utils/error_handling/failure_entity.dart';
+import 'package:weather_app/modules/shared/base/base_view.dart';
+import 'package:weather_app/modules/shared/widgets/loading.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy.dart';
+import 'package:weather_app/modules/weather/astronomy/presentation/bloc/astronomy_bloc.dart';
 import 'package:weather_app/modules/weather/astronomy/presentation/widgets/astronomy_info.dart';
 
-class AstronomyView extends StatelessWidget with BaseWidgetMixin {
-  AstronomyView({Key? key}) : super(key: key);
+class AstronomyView extends BaseView<AstronomyBloc, Astronomy> {
+  @override
+  void error(BuildContext context, FailureEntity entity,) {
+    context.go(Routes.ERROR, extra: entity);
+  }
 
   @override
-  Widget body(BuildContext context) {
+  Widget initial(BuildContext context, AstronomyBloc bloc,) {
+    bloc.add(const GetAstronomyData());
+
+    return const LoadingWidget();
+  }
+
+  @override
+  Widget success(BuildContext context, AstronomyBloc bloc, Astronomy data,) {
     return Container(
       padding: const EdgeInsets.symmetric(
         vertical: AppValues.padding,
