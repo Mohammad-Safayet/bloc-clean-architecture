6ce251b0e1d88acf22339c4e817924eec38a8f69
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-16 06:39:30 +0600

    added astronomy bloc

---
+++ lib/modules/weather/astronomy/presentation/bloc/astronomy_bloc.dart
@@ -0,0 +1,46 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:weather_app/core/config/config.dart';
+import 'package:weather_app/modules/shared/base/base_state.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy_query_param.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/repositories/astronomy_repo.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/usecases/get_astronomy_data_usecase.dart';
+
+part 'astronomy_event.dart';
+
+part 'astronomy_bloc.freezed.dart';
+
+class AstronomyBloc extends Bloc<AstronomyEvent, BaseState> {
+  final _logger = BuildConfig.instance.envConfig.logger;
+
+  final AstronomyRepository _repository;
+
+  AstronomyBloc(this._repository) : super(const Initial()) {
+    on<GetAstronomyData>(_getAstronomyData);
+  }
+
+  Future _getAstronomyData(event, emit) async {
+    emit(const Loading());
+
+    final usecase = GetAstronomyDataUsecase(
+      repository: _repository,
+    );
+
+    final result = await usecase.call(
+      AstronomyQueryParam(key: "key", q: "", dt: "dt"),
+    );
+    _logger.d(result);
+
+    result.fold((entity) {
+      emit(
+        Error(
+          error: entity,
+        ),
+      );
+    }, (data) {
+      emit(
+        Success(data: data),
+      );
+    });
+  }
+}
---
+++ lib/modules/weather/astronomy/presentation/bloc/astronomy_event.dart
@@ -0,0 +1,6 @@
+part of 'astronomy_bloc.dart';
+
+@freezed
+class AstronomyEvent with _$AstronomyEvent {
+  const factory AstronomyEvent.getAstronomyData() = GetAstronomyData;
+}
