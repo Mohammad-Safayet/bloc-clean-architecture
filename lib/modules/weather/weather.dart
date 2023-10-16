920af5b003a4e57ac2bb6d06b80f0f9e044020bd
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-16 08:14:52 +0600

    added Astronomy bloc provider

--- lib/modules/weather/weather.dart
+++ lib/modules/weather/weather.dart
@@ -4,6 +4,8 @@ import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/core/utils/di/di.dart';
 
 import 'package:weather_app/modules/shared/base/base_screen.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/repositories/astronomy_repo.dart';
+import 'package:weather_app/modules/weather/astronomy/infra/datasources/astronomy_remote_datasource.dart';
 import 'package:weather_app/modules/weather/astronomy/presentation/bloc/astronomy_bloc.dart';
 import 'package:weather_app/modules/weather/main/presentation/bloc/connectivity_bloc.dart';
 import 'package:weather_app/modules/weather/main/presentation/views/weather_screen.dart';
@@ -29,6 +31,11 @@ class WeatherPage extends BaseScreen {
       providers: [
         BlocProvider(
           create: (context) => ConnectivityBloc(),
+        ),
+        BlocProvider(
+          create: (context) => AstronomyBloc(
+            DependencyInjection.instance.getInstance<AstronomyRepository>(),
+          ),
         ),
       ],
       child: WeatherScreenView(
