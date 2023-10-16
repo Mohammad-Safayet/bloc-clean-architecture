547cdb159c921ba28ad2f7a9a9aac9c18057ae97
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-16 06:53:12 +0600

    added q and dt fields

--- lib/modules/weather/astronomy/domain/usecases/get_astronomy_data_usecase.dart
+++ lib/modules/weather/astronomy/domain/usecases/get_astronomy_data_usecase.dart
@@ -2,26 +2,48 @@ import 'package:dartz/dartz.dart';
 import 'package:weather_app/core/utils/error_handling/error_handling.dart';
 import 'package:weather_app/modules/shared/base/base_usecase.dart';
 import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy_query_param.dart';
 import 'package:weather_app/modules/weather/main/infra/repositories/weather_repo.dart';
 
 class GetAstronomyDataUsecase extends BaseUsecase<Astronomy> {
   final WeatherRepository repository;
 
+  String _q = "";
+  String _dt = "";
+
   GetAstronomyDataUsecase({
     required this.repository,
   });
+
+  set q(String q) {
+    _q = q;
+  }
+
+  set dt(String dt) {
+    _dt = dt;
+  }
 
   @override
   Future<Either<FailureEntity, Astronomy>> call() async {
     try {
       final result = await repository.getData(
-        "Dhaka",
-        {"dt": "2023-10-03"},
+        _q,
+        {"dt": _dt},
       );
 
       return Right(result);
     } catch (exception) {
-      final entity = mapExceptionToFailureEntity(exception as Exception);
+      logger.e(exception.toString());
+
+      final FailureEntity entity;
+      if (exception is Exception) {
+        entity = mapExceptionToFailureEntity(exception);
+      } else {
+        entity = const ApplicationFailure(
+          title: "Error Occurred",
+          message: "An Unexpected error occurred.",
+        );
+      }
 
       return Left(entity);
     }
