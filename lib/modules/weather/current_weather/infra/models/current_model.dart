f9defc683dba853fd4679273e3170966daf5a5ef
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-17 06:52:01 +0600

    renamed current_weather_model to current_model

---
+++ lib/modules/weather/current_weather/infra/models/current_model.dart
@@ -0,0 +1,79 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:weather_app/modules/weather/current_weather/infra/models/condition_model.dart';
+
+part 'weather_current_model.g.dart';
+
+@JsonSerializable()
+class CurrentWeatherModel {
+  @JsonKey(name: "last_updated")
+  final DateTime lastUpdated;
+  @JsonKey(name: "temp_c")
+  final double tempC;
+  @JsonKey(name: "temp_f")
+  final double tempF;
+  @JsonKey(name: "is_day")
+  final bool isDay;
+  final ConditionModel condition;
+  @JsonKey(name: "wind_mph")
+  final double windMph;
+  @JsonKey(name: "wind_kph")
+  final double windKph;
+  @JsonKey(name: "wind_degree")
+  final int windDegree;
+  @JsonKey(name: "wind_dir")
+  final String windDir;
+  @JsonKey(name: "pressure_mb")
+  final double pressureMb;
+  @JsonKey(name: "pressure_in")
+  final double pressureIn;
+  @JsonKey(name: "precip_mm")
+  final double precipMm;
+  @JsonKey(name: "precip_in")
+  final double precipIn;
+  final int cloud;
+  final int humidity;
+  @JsonKey(name: "feelslike_c")
+  final double feelslikeC;
+  @JsonKey(name: "feelslike_f")
+  final double feelslikeF;
+  @JsonKey(name: "vis_km")
+  final double visKm;
+  @JsonKey(name: "vis_miles")
+  final double visMiles;
+  final double uv;
+  @JsonKey(name: "gust_mph")
+  final double gustMph;
+  @JsonKey(name: "gust_kph")
+  final double gustKph;
+
+  CurrentWeatherModel({
+    required this.lastUpdated,
+    required this.tempC,
+    required this.tempF,
+    required this.isDay,
+    required this.condition,
+    required this.windMph,
+    required this.windKph,
+    required this.windDegree,
+    required this.windDir,
+    required this.pressureMb,
+    required this.pressureIn,
+    required this.precipMm,
+    required this.precipIn,
+    required this.cloud,
+    required this.humidity,
+    required this.feelslikeC,
+    required this.feelslikeF,
+    required this.visKm,
+    required this.visMiles,
+    required this.uv,
+    required this.gustMph,
+    required this.gustKph,
+  });
+
+  factory CurrentWeatherModel.fromJson(Map<String, dynamic> json) =>
+      _$CurrentWeatherModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CurrentWeatherModelToJson(this);
+}
