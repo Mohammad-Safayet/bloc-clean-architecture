a8589dbf2676cfcc562c0fa7a1143d79fbb9184a
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-17 07:56:02 +0600

    implemented get today's astronomy data usecase

---
+++ lib/modules/weather/astronomy/domain/usecases/get_today_astronomy_data_usecase.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:weather_app/core/utils/error_handling/error_handling.dart';
+import 'package:weather_app/modules/shared/base/base_usecase.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/repositories/astronomy_repo.dart';
+
+class GetTodayAstronomyDataUsecase extends BaseUsecase<Astronomy> {
+  final AstronomyRepository repository;
+
+  String _q = "";
+
+  GetTodayAstronomyDataUsecase({
+    required this.repository,
+  });
+
+  set q(String q) {
+    _q = q;
+  }
+
+  @override
+  Future<Either<FailureEntity, Astronomy>> call() async {
+    try {
+      final result = await repository.getTodayData(_q);
+
+      return Right(result);
+    } catch (exception) {
+      logger.e(exception.toString());
+
+      final FailureEntity entity;
+      if (exception is Exception) {
+        entity = mapExceptionToFailureEntity(exception);
+      } else {
+        entity = const ApplicationFailure(
+          title: "Error Occurred",
+          message: "An Unexpected error occurred.",
+        );
+      }
+
+      return Left(entity);
+    }
+  }
+}
