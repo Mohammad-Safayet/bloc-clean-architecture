32c598ead57c2ce8aa31b391be157ae6e9832f75
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-17 07:55:01 +0600

    added today and tomorrow data

--- lib/modules/weather/astronomy/domain/repositories/astronomy_repo.dart
+++ lib/modules/weather/astronomy/domain/repositories/astronomy_repo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:logger/logger.dart';
 import 'package:weather_app/core/config/config.dart';
+import 'package:weather_app/core/constants/app_values.dart';
+import 'package:weather_app/core/extensions/datetime_extension.dart';
 import 'package:weather_app/core/extensions/models/astronomy_model_extension.dart';
 import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy.dart';
 import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy_query_param.dart';
@@ -15,22 +17,52 @@ class AstronomyRepository extends WeatherRepository<Astronomy> {
   AstronomyRepository({required this.remoteDataSource});
 
   @override
-  Future<Astronomy> getData(String q, Map<String, dynamic> other) async {
+  Future<Astronomy> getData(
+    dynamic query,
+  ) async {
     try {
-      final apiKey = dotenv.env["API_KEY"]!;
-
-      final query = AstronomyQueryParam(
-        key: apiKey,
-        q: q,
-        dt: other["dt"],
-      );
-
       final AstronomyModel astronomyModel =
           await remoteDataSource.getData(query);
 
       final astronomy = astronomyModel.toEntity();
 
       return astronomy;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Astronomy> getTodayData(String q) {
+    try {
+      final now = DateTime.now();
+      final apiKey = dotenv.env["API_KEY"]!;
+
+      final query = AstronomyQueryParam(
+        key: apiKey,
+        q: q,
+        dt: now.toQString(),
+      );
+
+      return getData(query);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Astronomy> getTomorrowData(String q) {
+    try {
+      final tmr = DateTime.now().add(
+        AppValues.duration1D,
+      );
+      final apiKey = dotenv.env["API_KEY"]!;
+
+      final query = AstronomyQueryParam(
+        key: apiKey,
+        q: q,
+        dt: tmr.toQString(),
+      );
+
+      return getData(query);
     } catch (e) {
       rethrow;
     }
