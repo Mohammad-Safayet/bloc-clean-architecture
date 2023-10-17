53ffbcfd238da3e56aa8543b09cc8b3f19847dfe
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-17 07:57:49 +0600

    implemented trigger for new event and renamed event

--- lib/modules/weather/astronomy/presentation/bloc/astronomy_bloc.dart
+++ lib/modules/weather/astronomy/presentation/bloc/astronomy_bloc.dart
@@ -1,10 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
 import 'package:weather_app/core/config/config.dart';
 import 'package:weather_app/modules/shared/base/base_state.dart';
-import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy_query_param.dart';
 import 'package:weather_app/modules/weather/astronomy/domain/repositories/astronomy_repo.dart';
-import 'package:weather_app/modules/weather/astronomy/domain/usecases/get_astronomy_data_usecase.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/usecases/get_today_astronomy_data_usecase.dart';
+import 'package:weather_app/modules/weather/astronomy/domain/usecases/get_tomorrow_astronomy_data_usecase.dart';
 
 part 'astronomy_event.dart';
 
@@ -16,17 +17,41 @@ class AstronomyBloc extends Bloc<AstronomyEvent, BaseState> {
   final AstronomyRepository _repository;
 
   AstronomyBloc(this._repository) : super(const Initial()) {
-    on<GetAstronomyData>(_getAstronomyData);
+    on<GetTodayAstronomyData>(_getTodayAstronomyData);
+    on<GetTomorrowAstronomyData>(_getTomorrowAstronomyData);
   }
 
-  Future _getAstronomyData(event, emit) async {
+  Future _getTodayAstronomyData(event, emit) async {
     emit(const Loading());
 
-    final usecase = GetAstronomyDataUsecase(
+    final usecase = GetTodayAstronomyDataUsecase(
       repository: _repository,
     );
     usecase.q = event.q;
-    usecase.dt = event.dt;
+
+    final result = await usecase.call();
+    _logger.d(result);
+
+    result.fold((entity) {
+      emit(
+        Error(
+          error: entity,
+        ),
+      );
+    }, (data) {
+      emit(
+        Success(data: data),
+      );
+    });
+  }
+
+  Future _getTomorrowAstronomyData(event, emit) async {
+    emit(const Loading());
+
+    final usecase = GetTomorrowAstronomyDataUsecase(
+      repository: _repository,
+    );
+    usecase.q = event.q;
 
     final result = await usecase.call();
     _logger.d(result);
