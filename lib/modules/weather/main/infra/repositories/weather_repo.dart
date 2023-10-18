318f686d7a9a85c4a07b503292fcb02537b64b5e
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-18 07:39:33 +0600

    added convert and WeatherRemoteDataSource field

--- lib/modules/weather/main/infra/repositories/weather_repo.dart
+++ lib/modules/weather/main/infra/repositories/weather_repo.dart
@@ -1,8 +1,20 @@
-import 'package:weather_app/modules/weather/astronomy/domain/entities/astronomy.dart';
+import 'package:logger/logger.dart';
+import 'package:weather_app/core/config/config.dart';
+import 'package:weather_app/modules/weather/main/infra/datasources/weather_remote_datasource.dart';
 
-abstract class WeatherRepository<T> {
-  Future<T> getData(
-    String q,
-    Map<String, dynamic> other,
-  );
+abstract class WeatherRepository<T extends Object> {
+  final Logger logger = BuildConfig.instance.envConfig.logger;
+
+  WeatherRemoteDataSource get remoteDataSource;
+
+  Future<T> getData(dynamic query) async {
+    final model = await remoteDataSource.getData(query);
+
+    final T entity = convert(model);
+    logger.d(entity);
+
+    return entity;
+  }
+
+  T convert(dynamic model);
 }
