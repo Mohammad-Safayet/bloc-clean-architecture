9dffd8e5be3ea9922c4b17aa72fdefe8f33d9ced
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-18 07:54:00 +0600

    added current_weather_repository_impl

---
+++ lib/modules/weather/current_weather/domain/repositories/current_weather_repository_impl.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:weather_app/core/extensions/models/current_weather/weather_model_extension.dart';
+import 'package:weather_app/modules/weather/current_weather/domain/entities/weather.dart';
+import 'package:weather_app/modules/weather/current_weather/domain/entities/weather_query_param.dart';
+import 'package:weather_app/modules/weather/current_weather/infra/models/weather_model.dart';
+import 'package:weather_app/modules/weather/current_weather/infra/repositories/current_weather_repository.dart';
+import 'package:weather_app/modules/weather/main/infra/datasources/weather_remote_datasource.dart';
+import 'package:weather_app/modules/weather/main/infra/repositories/weather_repo.dart';
+
+class CurrentWeatherRepositoryImpl extends WeatherRepository<Weather>
+    implements CurrentWeatherRepository {
+  @override
+  final WeatherRemoteDataSource remoteDataSource;
+
+  CurrentWeatherRepositoryImpl(
+    this.remoteDataSource,
+  );
+
+  @override
+  Weather convert(model) {
+    final entity = (model as WeatherModel).toEntity();
+
+    return entity;
+  }
+
+  @override
+  Future<Weather> getCurrentWeatherData(String query) {
+    try {
+      final apiKey = dotenv.env["API_KEY"]!;
+
+      final param = WeatherQueryParam(
+        key: apiKey,
+        q: query,
+        aqi: "no",
+      );
+
+      return getData(param);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
