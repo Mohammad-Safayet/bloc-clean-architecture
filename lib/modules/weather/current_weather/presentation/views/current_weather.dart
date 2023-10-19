a9ae1f340a8b4c748df61846dece1ba27e6bc7c5
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-19 06:46:38 +0600

    added bloc to current_weather view

--- lib/modules/weather/current_weather/presentation/views/current_weather.dart
+++ lib/modules/weather/current_weather/presentation/views/current_weather.dart
@@ -1,19 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:go_router/go_router.dart';
+import 'package:logger/logger.dart';
 import 'package:weather_app/core/constants/app_values.dart';
+import 'package:weather_app/core/extensions/position_extension.dart';
+import 'package:weather_app/core/routes/app_pages.dart';
+import 'package:weather_app/core/utils/error_handling/failure_entity.dart';
+import 'package:weather_app/modules/shared/base/base_view.dart';
 import 'package:weather_app/modules/shared/mixin/base_widget.dart';
+import 'package:weather_app/modules/shared/widgets/loading.dart';
+import 'package:weather_app/modules/weather/current_weather/domain/entities/weather.dart';
+import 'package:weather_app/modules/weather/current_weather/domain/entities/weather_query_param.dart';
+import 'package:weather_app/modules/weather/current_weather/domain/repositories/current_weather_repository_impl.dart';
+import 'package:weather_app/modules/weather/current_weather/domain/usecases/get_current_weather_data_usecase.dart';
+import 'package:weather_app/modules/weather/current_weather/infra/datasources/current_weather_remote_datasource.dart';
+import 'package:weather_app/modules/weather/current_weather/presentation/bloc/current_weather_bloc.dart';
 import 'package:weather_app/modules/weather/current_weather/presentation/widgets/weather_display.dart';
 import 'package:weather_app/modules/weather/current_weather/presentation/widgets/weather_info.dart';
 import 'package:weather_app/modules/weather/current_weather/presentation/widgets/weather_screen_bar.dart';
 import 'package:weather_app/modules/weather/current_weather/presentation/widgets/view_details_button.dart';
 import 'package:weather_app/modules/weather/current_weather/presentation/widgets/weather_title.dart';
 
-class CurrentWeatherView extends StatelessWidget with BaseWidgetMixin {
-  CurrentWeatherView({Key? key}) : super(key: key);
+class CurrentWeatherView extends BaseView<CurrentWeatherBloc, Weather> {
+  final Position position;
+
+  CurrentWeatherView({
+    super.key,
+    required this.position,
+  });
 
   @override
-  Widget body(BuildContext context) {
-    return  Container(
-      child: const  Column(
+  void error(
+    BuildContext context,
+    FailureEntity entity,
+  ) {
+    context.go(Routes.ERROR, extra: entity);
+  }
+
+  @override
+  Widget initial(
+    BuildContext context,
+    CurrentWeatherBloc bloc,
+  ) {
+    bloc.add(
+      GetCurrentWeatherData(
+        q: position.toQString(),
+      ),
+    );
+
+    return const LoadingWidget();
+  }
+
+  @override
+  Widget success(
+    BuildContext context,
+    CurrentWeatherBloc bloc,
+    Weather data,
+  ) {
+    return const SizedBox(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           SizedBox(
