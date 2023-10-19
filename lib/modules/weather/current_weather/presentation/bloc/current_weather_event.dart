adf201a446348a289719c52003bb51955bfbbade
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-19 06:40:50 +0600

    added GetCurrentWeatherData event

--- lib/modules/weather/current_weather/presentation/bloc/current_weather_event.dart
+++ lib/modules/weather/current_weather/presentation/bloc/current_weather_event.dart
@@ -2,5 +2,7 @@ part of 'current_weather_bloc.dart';
 
 @freezed
 class CurrentWeatherEvent with _$CurrentWeatherEvent {
-
+  const factory CurrentWeatherEvent.getCurrentWeatherData({
+    required String q,
+  }) = GetCurrentWeatherData;
 }
