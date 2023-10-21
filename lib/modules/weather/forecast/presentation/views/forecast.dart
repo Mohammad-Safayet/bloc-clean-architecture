50912fd6fcd2e0ca342a0dcb45ade7743535f791
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-21 08:22:04 +0600

    implemented bloc consumer view

--- lib/modules/weather/forecast/presentation/views/forecast.dart
+++ lib/modules/weather/forecast/presentation/views/forecast.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:go_router/go_router.dart';
 import 'package:weather_app/core/constants/app_text_styles.dart';
 import 'package:weather_app/core/constants/app_values.dart';
+import 'package:weather_app/core/extensions/position_extension.dart';
+import 'package:weather_app/core/routes/app_pages.dart';
+import 'package:weather_app/core/utils/error_handling/failure_entity.dart';
+import 'package:weather_app/modules/shared/base/base_view.dart';
+import 'package:weather_app/modules/shared/widgets/loading.dart';
+import 'package:weather_app/modules/weather/forecast/domain/entities/forecast.dart';
+import 'package:weather_app/modules/weather/forecast/presentation/bloc/forecast_bloc.dart';
 import 'package:weather_app/modules/weather/forecast/presentation/widgets/forecast_card.dart';
 
-class ForecastView extends StatelessWidget {
-  const ForecastView({Key? key}) : super(key: key);
+class ForecastView extends BaseView<ForecastBloc, Forecast> {
+  final Position position;
+
+  ForecastView({
+    super.key,
+    required this.position,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    final list = [
-      "Saturday",
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-    ];
+  void error(BuildContext context, FailureEntity entity) {
+    context.go(Routes.ERROR, extra: entity);
+  }
+
+  @override
+  Widget initial(BuildContext context, ForecastBloc bloc) {
+    bloc.add(
+      GetForecastData(
+        query: position.toQString(),
+        days: 7,
+      ),
+    );
+
+    return const LoadingWidget();
+  }
+
+  @override
+  Widget success(BuildContext context, ForecastBloc bloc, Forecast data) {
+    final list = data.forecastDays;
 
     return Container(
       padding: const EdgeInsets.symmetric(
@@ -32,7 +56,7 @@ class ForecastView extends StatelessWidget {
               bottom: AppValues.margin,
             ),
             child: const Text(
-              "Forecasts for next 7 days",
+              "Weather Forecast",
               style: AppTextStyles.title,
             ),
           ),
@@ -42,7 +66,8 @@ class ForecastView extends StatelessWidget {
               itemCount: list.length,
               itemBuilder: (context, index) {
                 return ForecastCardWidget(
-                  day: list[index],
+                  date: list[index].date,
+                  avgTemp: list[index].day.avgTempC,
                 );
               },
             ),
@@ -50,6 +75,5 @@ class ForecastView extends StatelessWidget {
         ],
       ),
     );
-    ;
   }
 }
