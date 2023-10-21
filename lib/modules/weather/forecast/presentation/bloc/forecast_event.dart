4a8679d471ca80eab9842e16067c0c0d264a51c0
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-21 07:06:19 +0600

    added getForecastData event

--- lib/modules/weather/forecast/presentation/bloc/forecast_event.dart
+++ lib/modules/weather/forecast/presentation/bloc/forecast_event.dart
@@ -2,5 +2,5 @@ part of 'forecast_bloc.dart';
 
 @freezed
 class ForecastEvent with _$ForecastEvent {
-  const factory ForecastEvent.started() = _Started;
+  const factory ForecastEvent.getForecastData() = GetForecastData;
 }
