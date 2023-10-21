f842371866a2f6c39fc0a197abfe9681c83fa924
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-21 07:08:43 +0600

    added params in getForecastData event

--- lib/modules/weather/forecast/presentation/bloc/forecast_event.dart
+++ lib/modules/weather/forecast/presentation/bloc/forecast_event.dart
@@ -2,5 +2,8 @@ part of 'forecast_bloc.dart';
 
 @freezed
 class ForecastEvent with _$ForecastEvent {
-  const factory ForecastEvent.getForecastData() = GetForecastData;
+  const factory ForecastEvent.getForecastData({
+    required String query,
+    required int days,
+  }) = GetForecastData;
 }
