4fa627db6e3a4c5eb742d9c5e95f312ae25ade85
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-21 07:06:43 +0600

    implemented trigger for  getForecastData event

--- lib/modules/weather/forecast/presentation/bloc/forecast_bloc.dart
+++ lib/modules/weather/forecast/presentation/bloc/forecast_bloc.dart
@@ -1,14 +1,43 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:weather_app/core/config/config.dart';
 import 'package:weather_app/modules/shared/base/base_state.dart';
+import 'package:weather_app/modules/weather/forecast/domain/usecases/get_forecast_data_usecase.dart';
+import 'package:weather_app/modules/weather/forecast/infra/repositories/forecast_repository.dart';
 
 part 'forecast_event.dart';
+
 part 'forecast_bloc.freezed.dart';
 
 class ForecastBloc extends Bloc<ForecastEvent, BaseState> {
-  ForecastBloc() : super(Initial()) {
-    on<ForecastEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final _logger = BuildConfig.instance.envConfig.logger;
+
+  final ForecastRepository repository;
+
+  ForecastBloc(this.repository) : super(const Initial()) {
+    on<GetForecastData>(_getForecastData);
+  }
+
+  Future _getForecastData(event, emit) async {
+    emit(const Loading());
+
+    final getForecastDataUsecase = GetForecastDataUsecase(
+      repository: repository,
+    );
+
+    final result = await getForecastDataUsecase.call();
+
+    result.fold(
+      (left) => emit(
+        Error(
+          error: left,
+        ),
+      ),
+      (right) => emit(
+        Success(
+          data: right,
+        ),
+      ),
+    );
   }
 }
