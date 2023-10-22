6999c638efed2bba1721c79a83d65fdd5f49c362
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-22 08:14:23 +0600

    added arrow button

--- lib/modules/weather/forecast/presentation/widgets/forecast_card_details.dart
+++ lib/modules/weather/forecast/presentation/widgets/forecast_card_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:weather_app/core/constants/app_text_styles.dart';
+import 'package:weather_app/core/constants/app_values.dart';
 import 'package:weather_app/core/extensions/datetime_extension.dart';
 import 'package:weather_app/modules/shared/mixin/base_widget.dart';
 
@@ -17,7 +19,7 @@ class ForecastCardDetailsWidget extends StatelessWidget with BaseWidgetMixin {
   @override
   Widget? body(BuildContext context) {
     return Row(
-
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -35,6 +37,20 @@ class ForecastCardDetailsWidget extends StatelessWidget with BaseWidgetMixin {
             ),
           ],
         ),
+        const Spacer(),
+        OutlinedButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<CircleBorder>(
+              const CircleBorder(),
+            ),
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {},
+          child: const FaIcon(
+            FontAwesomeIcons.arrowRight,
+            size: AppValues.icon_18,
+          ),
+        )
       ],
     );
   }
