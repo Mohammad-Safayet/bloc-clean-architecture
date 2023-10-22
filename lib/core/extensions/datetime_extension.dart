fb462d2b3d12e81a955c09ad218bd1bcf41a29cb
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-22 07:25:09 +0600

    added new extension method to compare time with other time

--- lib/core/extensions/datetime_extension.dart
+++ lib/core/extensions/datetime_extension.dart
@@ -2,4 +2,12 @@ extension DateTimeExtension on DateTime {
   String toQString() {
     return "$year-$month-$day";
   }
+
+  bool isSameTimeLoose(DateTime other) {
+    return day == other.day &&
+        month == other.month &&
+        year == other.year &&
+        hour == other.hour &&
+        (minute == other.minute || minute + 1 == other.minute);
+  }
 }
