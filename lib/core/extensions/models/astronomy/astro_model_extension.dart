02cfb1a86952180cb21fb37831f9ce3881d8b49f
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-22 07:24:17 +0600

    bug fix: No moonset bug fixed

--- lib/core/extensions/models/astronomy/astro_model_extension.dart
+++ lib/core/extensions/models/astronomy/astro_model_extension.dart
@@ -6,10 +6,18 @@ import 'package:weather_app/modules/weather/astronomy/infra/models/astro_model.d
 extension AstroModelExtension on AstroModel {
   Astro toEntity() {
     return Astro(
-      sunRise: DateFormat.jm().parseLoose(sunRise),
-      sunSet: DateFormat.jm().parseLoose(sunSet),
-      moonRise: DateFormat.jm().parseLoose(moonRise),
-      moonSet: DateFormat.jm().parseLoose(moonSet),
+      sunRise: (moonSet == "No sunrise")
+          ? DateTime.now()
+          : DateFormat.jm().parseLoose(sunRise),
+      sunSet: (moonSet == "No sunset")
+          ? DateTime.now()
+          : DateFormat.jm().parseLoose(sunSet),
+      moonRise: (moonSet == "No moonrise")
+          ? DateTime.now()
+          : DateFormat.jm().parseLoose(moonRise),
+      moonSet: (moonSet == "No moonset")
+          ? DateTime.now()
+          : DateFormat.jm().parseLoose(moonSet),
       moonPhase: moonPhase,
       moonIllumination: int.parse(moonIllumination),
       isMoonUp: isMoonUp == 1,
