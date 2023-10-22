a27316f3f69550879eee69ac93d3189b2aa322ea
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-22 07:37:21 +0600

    added extension to compare the same date

--- lib/core/extensions/datetime_extension.dart
+++ lib/core/extensions/datetime_extension.dart
@@ -10,4 +10,8 @@ extension DateTimeExtension on DateTime {
         hour == other.hour &&
         (minute == other.minute || minute + 1 == other.minute);
   }
+
+  bool isSameDate(DateTime other) {
+    return day == other.day && month == other.month && year == other.year;
+  }
 }
