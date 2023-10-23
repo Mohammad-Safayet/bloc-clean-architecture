c280e01da6eccd5a77dd4a6d7faae64da6210211
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-23 06:49:41 +0600

    added weather_details screen view

---
+++ lib/modules/weather_details/presentation/views/weather_details_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/core/constants/app_values.dart';
+import 'package:weather_app/modules/shared/base/base_screen.dart';
+import 'package:weather_app/modules/shared/widgets/divider.dart';
+import 'package:weather_app/modules/weather_details/presentation/widgets/weather_details_appbar.dart';
+import 'package:weather_app/modules/weather_details/presentation/widgets/weather_details_header.dart';
+import 'package:weather_app/modules/weather_details/presentation/widgets/weather_details_temp.dart';
+
+class WeatherDetailScreen extends BaseScreen {
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return const WeatherDetailsAppBarWidget(
+      appTitle: "lak",
+    );
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      margin: const EdgeInsets.only(
+        top: AppValues.margin,
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(
+            AppValues.radiusXLarge,
+          ),
+          topRight: Radius.circular(
+            AppValues.radiusXLarge,
+          ),
+        ),
+      ),
+      child: Container(),
+    );
+  }
+}
