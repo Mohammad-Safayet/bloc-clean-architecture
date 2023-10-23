a8d8a7dcf2083da09dd4248d0a10cefce1a1ca2a
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-23 09:47:31 +0600

    current weather: added button action

--- lib/modules/weather/current_weather/presentation/widgets/view_details_button.dart
+++ lib/modules/weather/current_weather/presentation/widgets/view_details_button.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:weather_app/core/constants/app_values.dart';
+import 'package:weather_app/core/routes/app_pages.dart';
 import 'package:weather_app/modules/shared/mixin/base_widget.dart';
+import 'package:weather_app/modules/weather/current_weather/domain/entities/weather.dart';
 
 class ViewDetailsButtonWidget extends StatelessWidget with BaseWidgetMixin {
-  ViewDetailsButtonWidget({Key? key}) : super(key: key);
+  final Weather weather;
+
+  ViewDetailsButtonWidget({
+    Key? key,
+    required this.weather,
+  }) : super(key: key);
 
   @override
   Widget body(BuildContext context) {
     return Align(
       alignment: Alignment.centerRight,
       child: OutlinedButton(
-        onPressed: () {},
+        onPressed: () {
+          context.push(
+            Routes.WEATHER_DETAILS,
+            extra: weather,
+          );
+        },
         style: OutlinedButton.styleFrom(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(
