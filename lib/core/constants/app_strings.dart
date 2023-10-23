95622d31d8ff8719b19e3793437fda4dbe422221
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-23 08:09:29 +0600

    added weather string constants

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,14 @@
+abstract class WeatherFieldConstants {
+  static const celsius = "Celsius";
+  static const fahrenheit = "Fahrenheit";
+  static const feelsLike = "Feels Like";
+  static const wind = "Wind";
+  static const windDegree = "Wind Degree";
+  static const windDir = "Wind Direction";
+  static const pressure = "Pressure";
+  static const precipitation  = "Precipitation ";
+  static const humidity = "Humidity";
+  static const clouds = "Clouds";
+  static const uv = "UV Index";
+  static const gust = "Gust";
+}
