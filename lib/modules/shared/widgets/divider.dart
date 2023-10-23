a28141c9f37654146368db2da4426989ad1f4347
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-23 06:42:45 +0600

    changed divider thickness

--- lib/modules/shared/widgets/divider.dart
+++ lib/modules/shared/widgets/divider.dart
@@ -6,12 +6,10 @@ class DividerWidget extends StatelessWidget with BaseWidgetMixin {
   @override
   Widget? body(BuildContext context) {
     return Divider(
-      color:
-      Theme.of(context).colorScheme.inverseSurface.withOpacity(0.2),
-      thickness: AppValues.dividerThickness_2,
+      color: Theme.of(context).colorScheme.inverseSurface.withOpacity(0.2),
+      thickness: AppValues.dividerThickness,
       endIndent: AppValues.dividerIndent_5,
       indent: AppValues.dividerIndent_5,
     );
   }
-
 }
