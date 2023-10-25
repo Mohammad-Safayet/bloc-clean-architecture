9c2c3d780a6b7aefdcc4816f2715ecc8f57f3d01
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-25 08:14:46 +0600

    documentation: BaseRemoteDatasource

--- lib/modules/shared/base/base_remote_datasource.dart
+++ lib/modules/shared/base/base_remote_datasource.dart
@@ -1,3 +1,7 @@
+/// Author: Safayet Latif
+/// Date: October 25, 2023
+/// Description: This abstract class, `BaseRemoteDataSource`, defines the interface for remote data sources in the context of network requests. It provides a base configuration for making API requests with error handling using the Dio package and network error handlers.
+
 import 'dart:io';
 
 import 'package:dio/dio.dart';
@@ -6,11 +10,19 @@ import 'package:weather_app/core/network/network_error_handlers.dart';
 import 'package:weather_app/core/network/network_provider.dart';
 import 'package:weather_app/core/utils/error_handling/error_handling.dart';
 
+/// An abstract class representing a remote data source for network requests.
 abstract class BaseRemoteDataSource {
+  /// The Dio client used for making API requests with headers.
   Dio get dioClient => NetworkProvider.dioWithHeaderToken;
 
+  /// The logger instance for logging network-related information.
   final logger = BuildConfig.instance.envConfig.logger;
 
+  /// Makes an API request with error handling and response parsing.
+  ///
+  /// This method takes a generic API request and handles Dio exceptions as well as custom error handling.
+  ///
+  /// Returns a [Response] containing the parsed API response data.
   Future<Response<T>> callApiWithErrorParser<T>(Future<Response<T>> api) async {
     try {
       Response<T> response = await api;
@@ -18,7 +30,7 @@ abstract class BaseRemoteDataSource {
       if (response.statusCode != HttpStatus.ok ||
           (response.data as Map<String, dynamic>)['statusCode'] !=
               HttpStatus.ok) {
-        // TODO
+        // TODO: Handle the response error.
       }
 
       return response;
