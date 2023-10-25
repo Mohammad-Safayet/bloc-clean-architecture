79cfb81be907e39f036630a3367e95060494cefc
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-25 08:17:46 +0600

    documentation: BaseView

--- lib/modules/shared/base/base_view.dart
+++ lib/modules/shared/base/base_view.dart
@@ -1,3 +1,7 @@
+/// Author: Safayet Latif
+/// Date: October 25, 2023
+/// Description: This abstract class, `BaseView`, defines the structure for creating views that interact with BLoCs (Business Logic Components) and handle various states, including initial, success, and error states. It provides a consistent structure for building widget-based views.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/core/constants/app_values.dart';
@@ -6,6 +10,7 @@ import 'package:weather_app/modules/shared/base/base_state.dart';
 import 'package:weather_app/modules/shared/mixin/base_widget.dart';
 import 'package:weather_app/modules/shared/widgets/loading.dart';
 
+/// An abstract class representing a base view in the Weather App.
 abstract class BaseView<B extends StateStreamable<BaseState>, T extends Object>
     extends StatelessWidget with BaseWidgetMixin {
   BaseView({Key? key}) : super(key: key);
@@ -38,19 +43,25 @@ abstract class BaseView<B extends StateStreamable<BaseState>, T extends Object>
     );
   }
 
+  /// Returns the initial state of the view.
   Widget initial(
-    BuildContext context,
-    B bloc,
-  );
+      BuildContext context,
+      B bloc,
+      );
 
+  /// Returns the view when the state is a success.
   Widget success(
-    BuildContext context,
-    B bloc,
-    T data,
-  );
+      BuildContext context,
+      B bloc,
+      T data,
+      );
 
+  /// Handles errors and provides a way to display error messages.
+  ///
+  /// - [context]: The `BuildContext` for the view.
+  /// - [entity]: The `FailureEntity` representing the error.
   void error(
-    BuildContext context,
-    FailureEntity entity,
-  );
+      BuildContext context,
+      FailureEntity entity,
+      );
 }
