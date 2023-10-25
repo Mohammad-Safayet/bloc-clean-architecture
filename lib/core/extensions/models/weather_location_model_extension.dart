32be8f61797fe7972936540319a33bf86df48098
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-25 07:57:26 +0600

    documentation: WeatherLocationModelExtension

--- lib/core/extensions/models/weather_location_model_extension.dart
+++ lib/core/extensions/models/weather_location_model_extension.dart
@@ -1,9 +1,19 @@
+/// Author: Mohammad Safayet Latif
+/// Date: October 25, 2023
+/// Description: This Dart extension provides a method for converting a WeatherLocationModel to a WeatherLocation entity.
 import 'package:intl/intl.dart';
 import 'package:weather_app/core/constants/app_values.dart';
 import 'package:weather_app/modules/weather/main/domain/entities/weather_location.dart';
 import 'package:weather_app/modules/weather/main/infra/models/weather_location_model.dart';
 
+/// A set of extension methods for converting a [WeatherLocationModel]
+/// to a [WeatherLocation] entity.
 extension WeatherLocationModelExtension on WeatherLocationModel {
+  /// Converts a [WeatherLocationModel] to a [WeatherLocation] entity.
+  ///
+  /// This method creates a [WeatherLocation] entity based on the properties of the [WeatherLocationModel].
+  /// It takes the name, region, country, latitude, longitude, tzId, and localTime properties from the model
+  /// and constructs a [WeatherLocation] entity. It also parses the localTime string using the provided date format.
   WeatherLocation toEntity() {
     return WeatherLocation(
       name: name,
