ef4ad06bcb04216d41f6984997801556a3a16391
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-25 07:56:49 +0600

    documentation: DateTimExtension

--- lib/core/extensions/datetime_extension.dart
+++ lib/core/extensions/datetime_extension.dart
@@ -1,8 +1,20 @@
+/// Author: Mohammad Safayet Latif
+/// Date: October 25, 2023
+/// Description: This Dart extension provides methods for working with DateTime objects.
+///
+/// A set of extension methods for the [DateTime] class.
 extension DateTimeExtension on DateTime {
+  /// Converts a [DateTime] object to a formatted string in "YYYY-MM-DD" format.
+  ///
+  /// Returns a string representing the year, month, and day of the [DateTime].
   String toQString() {
     return "$year-$month-$day";
   }
 
+  /// Checks if this [DateTime] is the same as another [DateTime] with some tolerance for minutes.
+  ///
+  /// Returns `true` if the day, month, year, and hour are the same, and the minutes are the same
+  /// or differ by one.
   bool isSameTimeLoose(DateTime other) {
     return day == other.day &&
         month == other.month &&
@@ -11,6 +23,9 @@ extension DateTimeExtension on DateTime {
         (minute == other.minute || minute + 1 == other.minute);
   }
 
+  /// Checks if this [DateTime] has the same date as another [DateTime].
+  ///
+  /// Returns `true` if the day, month, and year are the same.
   bool isSameDate(DateTime other) {
     return day == other.day && month == other.month && year == other.year;
   }
