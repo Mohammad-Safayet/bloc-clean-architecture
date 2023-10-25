8530d4d7520bd9343dae2f58e5ad9007d3a34981
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-25 08:00:02 +0600

    documentation: NetworkProvider

--- lib/core/network/network_provider.dart
+++ lib/core/network/network_provider.dart
@@ -1,16 +1,25 @@
+/// Author: Your Name
+/// Date: October 25, 2023
+/// Description: This Dart class provides network-related functionality using the Dio library, handles authentication tokens, and content type configuration.
 import 'package:dio/dio.dart';
 import 'package:weather_app/core/config/build_config.dart';
 import 'package:weather_app/core/network/network_request_header.dart';
 
+/// A class responsible for providing network-related functionality using the Dio library.
 class NetworkProvider {
   static Dio? _instance;
 
   static final BaseOptions _options = BaseOptions(
     baseUrl: BuildConfig.instance.envConfig.baseUrl,
-    connectTimeout: const Duration(seconds: 60,),
-    receiveTimeout: const Duration(seconds: 60,),
+    connectTimeout: const Duration(
+      seconds: 60,
+    ),
+    receiveTimeout: const Duration(
+      seconds: 60,
+    ),
   );
 
+  /// Returns a Dio instance for making HTTP requests.
   static Dio get httpDio {
     if (_instance == null) {
       _instance = Dio(_options);
@@ -23,12 +32,14 @@ class NetworkProvider {
     }
   }
 
+  /// Returns a Dio instance with added interceptors for handling authentication tokens.
   static Dio get tokenClient {
     _addInterceptors();
 
     return _instance!;
   }
 
+  /// Returns a Dio instance with added interceptors for handling authentication tokens.
   static Dio get dioWithHeaderToken {
     _addInterceptors();
 
@@ -41,15 +52,17 @@ class NetworkProvider {
     _instance!.interceptors.add(RequestHeaderInterceptor());
   }
 
-  static String _buildContentType(String version) {
-    return "user_defined_content_type+$version";
-  }
-
+  /// Sets the content type for requests based on the provided version.
   NetworkProvider.setContentType(String version) {
     _instance?.options.contentType = _buildContentType(version);
   }
 
+  /// Sets the content type for requests to "application/json".
   NetworkProvider.setContentTypeApplicationJson() {
     _instance?.options.contentType = "application/json";
+  }
+
+  String _buildContentType(String version) {
+    return "user_defined_content_type+$version";
   }
 }
